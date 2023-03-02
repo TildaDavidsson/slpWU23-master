1f5021ec4a05f424b7f9bba576916ceb161fc83b
Tilda Davidsson NTI <119052094+TildaDavidsson@users.noreply.github.com>
2023-03-02 20:31:06 +0100

    nyportfölj

--- test.js
+++ test.js
@@ -3,13 +3,13 @@ document.addEventListener('DOMContentLoaded', function() {
     $('.image').click(function() {
       $('.image').removeClass('clicked');
       $(this).addClass('clicked');
-      $('.text-container').removeClass('active');
-      $(this).next('.text-container').addClass('active');
     });
-    var text = $(this).attr('data-text');
     
-    // Set the text square content to the retrieved text
-    $('.text-square').text(text);
-  });
-});  
+    $(document).click(function(event) {
+      if (!$(event.target).hasClass('image')) {
+        $('.image').removeClass('clicked').removeAttr('style');
+      }
+    });
+  });  
+});
   
