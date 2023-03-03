41086e4afe1951a8dc313c8b40b6afa91889ccd5
Tilda Davidsson NTI <119052094+TildaDavidsson@users.noreply.github.com>
2023-03-03 13:39:01 +0100

    generellt fix
    graff, skate start

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
+document.addEventListener('DOMContentLoaded', function() {
+    $(document).ready(function() {
+      $('.image').click(function() {
+        $('.image').removeClass('clicked');
+        $(this).addClass('clicked');
+      });
+      
+      $(document).click(function(event) {
+        if (!$(event.target).hasClass('image')) {
+          $('.image').removeClass('clicked').removeAttr('style');
+        }
+      });
+    });  
+});
+
 let layer = document.querySelector(".fade-layer").addEventListener("click",showMenu)
 let button = document.querySelector(".menu-button").addEventListener("click",showMenu)
 
