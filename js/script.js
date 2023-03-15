c65a50ede669aa18969e3439dd93a591793a9a76
Tilda Davidsson NTI <119052094+TildaDavidsson@users.noreply.github.com>
2023-03-15 12:50:56 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -13,14 +13,3 @@ document.addEventListener('DOMContentLoaded', function() {
     });  
 });
 
-let layer = document.querySelector(".fade-layer").addEventListener("click",showMenu)
-let button = document.querySelector(".menu-button").addEventListener("click",showMenu)
-
-
-function showMenu(){
-    let menu = document.querySelector(".menu");
-    menu.classList.toggle("show");
-    
-    let layer = document.querySelector(".fade-layer");
-    layer.classList.toggle("visible");
-} 
